65aed129b53999e139b3f32847d590f1e4e7bd2a
patelhr <patelhrp7@gmail.com>
2021-01-08 11:24:11 +0530

    Init commit

---
+++ workflow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace workflow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
